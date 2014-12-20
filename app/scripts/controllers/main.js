e86a8c8cc5c46ac3d521418ba29e8a5619086d02
angelito rojas <angelsystem1387@gmail.com>
2014-12-20 00:25:04 -0500

    add modal

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,29 @@
  * Controller of the landingApp
  */
 angular.module('landingApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $modal) {
+
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.toggleModal = function(){
+      var modalInstance = $modal.open({
+        templateUrl: 'views/new_suscription.html',
+        controller: 'AddSuscriptionCtrl'
+      });
+    }
+  })
+
+  .controller('AddSuscriptionCtrl', function ($scope, $modalInstance) {
+    $scope.ok = function () {
+      $modalInstance.close('ok');
+    };
+
+
+    $scope.cancel = function () {
+      $modalInstance.dismiss('cancel');
+    };
   });
