2edd8d9b3661935461b952dd2f286bab83dfdf65
videmor <videmor@gmail.com>
2014-12-23 05:47:21 -0500

    :octocat: change rules en gruntfile clean

--- Gruntfile.js
+++ Gruntfile.js
@@ -143,7 +143,7 @@ module.exports = function (grunt) {
             '!<%= yeoman.dist %>/Procfile',
             '!<%= yeoman.dist %>/package.json',
             '!<%= yeoman.dist %>/server.js',
-            '!<%= yeoman.dist %>/node_modules'
+            '!<%= yeoman.dist %>/node_modules{,*/}*'
           ]
         }]
       },
