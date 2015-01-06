8ca32996efa9ebf37c39a4634cf1012f8dbbd004
videmor <videmor@gmail.com>
2015-01-06 13:45:31 -0500

    change meessages

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -20,11 +20,11 @@ angular.module('landingApp')
 
     $scope.courses = [
       {imagePath: url+'basic.png', name: 'basic', title: 'Básico', content: 'Manejaremos las herramientas fundamentales y sus diferencias entre ellas.'},
-      {imagePath: url+'javascript.png', name: 'javascript', title: 'Javascript', content: 'Aprenderás lo basíco de javascript para dar una mayor interactividad a tu sitio.'},
-      {imagePath: url+'html.png', name: 'html', title: 'HTML', content: 'Aprenderás lo basíco de javascript para dar una mayor interactividad a tu sitio.'},
-      {imagePath: url+'ruby.png', name: 'ruby', title: 'Ruby', content: 'Tu meta será aprender Ruby, su sintáxis, estructura, funciones y librerías.'},
-      {imagePath: url+'css.png', name: 'css', title: 'CSS', content: 'Podrás crear sitios impresionantes, todo lo que necesitas es un poco de creatividad y tener un entendimiento sólido del css.'},
-      {imagePath: url+'extra.png', name: 'extra', title: 'Extra', content: 'Verás como hacer prototipos funcionales para smartphone, crear sitios dínamicos con Ruby on Rails y algo de angular.'}
+      {imagePath: url+'javascript.png', name: 'javascript', title: 'Javascript', content: 'Conoceras lo básico de javascript para dar una mayor interactividad a tu sitio.'},
+      {imagePath: url+'html.png', name: 'html', title: 'HTML', content: 'Aprenderás a estructurar de manera sencilla tu sitio web.'},
+      {imagePath: url+'ruby.png', name: 'ruby', title: 'Ruby', content: 'Exploraremos el lenguaje Ruby su sintáxis, estructura, funciones y librerías.'},
+      {imagePath: url+'css.png', name: 'css', title: 'CSS', content: 'Conoceremos como darle estilo a nuestro sitio.'},
+      {imagePath: url+'extra.png', name: 'extra', title: 'Extra', content: 'Verás como hacer prototipos funcionales para smartphone, crear sitios dínamicos con Ruby on Rails.'}
     ]
 
     $scope.toggleModal = function(){
