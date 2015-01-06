5d6ad127b2fe788f2131811ecabb04ac12421140
angelito rojas <angelsystem1387@gmail.com>
2015-01-05 23:48:59 -0500

    images dynamic

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -16,15 +16,15 @@ angular.module('landingApp')
       'Karma'
     ];
 
-    $scope.url = 'images/logos/'
+    url = 'images/logos/'
 
     $scope.courses = [
-      {name: 'basic', title: 'Básico', content: 'Manejaremos las herramientas fundamentales y sus diferencias entre ellas.'},
-      {name: 'javascript', title: 'Javascript', content: 'Aprenderás lo basíco de javascript para dar una mayor interactividad a tu sitio.'},
-      {name: 'html', title: 'HTML', content: 'Aprenderás lo basíco de javascript para dar una mayor interactividad a tu sitio.'},
-      {name: 'ruby', title: 'Ruby', content: 'Tu meta será aprender Ruby, su sintáxis, estructura, funciones y librerías.'},
-      {name: 'css', title: 'CSS', content: 'Podrás crear sitios impresionantes, todo lo que necesitas es un poco de creatividad y tener un entendimiento sólido del css.'},
-      {name: 'extra', title: 'Extra', content: 'Verás como hacer prototipos funcionales para smartphone, crear sitios dínamicos con Ruby on Rails y algo de angular.'}
+      {imagePath: url+'basic.png', name: 'basic', title: 'Básico', content: 'Manejaremos las herramientas fundamentales y sus diferencias entre ellas.'},
+      {imagePath: url+'javascript.png', name: 'javascript', title: 'Javascript', content: 'Aprenderás lo basíco de javascript para dar una mayor interactividad a tu sitio.'},
+      {imagePath: url+'html.png', name: 'html', title: 'HTML', content: 'Aprenderás lo basíco de javascript para dar una mayor interactividad a tu sitio.'},
+      {imagePath: url+'ruby.png', name: 'ruby', title: 'Ruby', content: 'Tu meta será aprender Ruby, su sintáxis, estructura, funciones y librerías.'},
+      {imagePath: url+'css.png', name: 'css', title: 'CSS', content: 'Podrás crear sitios impresionantes, todo lo que necesitas es un poco de creatividad y tener un entendimiento sólido del css.'},
+      {imagePath: url+'extra.png', name: 'extra', title: 'Extra', content: 'Verás como hacer prototipos funcionales para smartphone, crear sitios dínamicos con Ruby on Rails y algo de angular.'}
     ]
 
     $scope.toggleModal = function(){
